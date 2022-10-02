e4a926bc8fd9aa64da797b147c3ea543d665ccc8
akglitch <mcaninakwa@gmail.com>
2022-10-02 16:26:06 -0400

    initial commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const items = [
+    {
+        "img":require("./components/alex-knight-2EJCSULRwC8-unsplash.jpg")
+    },
+    {
+        "img":require("./components/jason-blackeye-nyL-rzwP-Mk-unsplash.jpg")
+    },
+    {
+        "img":require("./components/arseny-togulev-MECKPoKJYjM-unsplash.jpg")
+    },
+    {
+        "img":require("./components/alex-knight-2EJCSULRwC8-unsplash.jpg")
+    }
+
+]
+

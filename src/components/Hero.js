f3511647730137266b3cce856083c8c3314c33c6
akglitch <mcaninakwa@gmail.com>
2022-10-02 13:49:22 -0400

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import '../App.css' 
+
+
+function Hero() {
+    return ( 
+        <div className="bg">
+            <div className="pattern">
+                
+            </div>
+
+        </div>
+     );
+}
+
+export default Hero;
